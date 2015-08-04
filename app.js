e45fbe1dedca8f7e33a9332de8e15a892ccbbd29
zehui.lv <zehui.lv@dianrong.com>
2015-08-04 16:39:37 +0800

    add port and fix bug in package.json

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//set port
+app.set('port', process.env.PORT || 3000);
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
