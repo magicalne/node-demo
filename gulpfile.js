7ead974d44c617c45e72bc1e10d846a84e7b37f4
zehui.lv <zehui.lv@dianrong.com>
2015-08-05 14:53:11 +0800

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+
+gulp.task('less', function() {
+  return gulp.src('./public/stylesheets/less/**/*.less')
+    .pipe(less({
+      paths: [path.join(__dirname, 'less', 'includes')]
+    }))
+    .pipe(gulp.dest('./public/stylesheets/css'));
+});
+
+gulp.task('default', function() {
+
+});
