3214783165dc8c94df5749f34b8f3a6f276156a6
zehui.lv <zehui.lv@dianrong.com>
2015-08-06 11:28:25 +0800

    use bower.json to config and modify bower install directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var uglify = require('gulp-uglify');
 var path = require('path');
+var concat = require('gulp-concat');
+
+var compiledPath = './public/compiled';
 
 gulp.task('less', function() {
   return gulp.src('./public/stylesheets/less/**/*.less')
@@ -8,6 +12,16 @@ gulp.task('less', function() {
       paths: [path.join(__dirname, 'less', 'includes')]
     }))
     .pipe(gulp.dest('./public/stylesheets/css'));
+});
+
+gulp.task('thirdparty', function() {
+  var basePath = './public/components/';
+  return gulp.src([basePath + 'jquery/jquery.js',
+      basePath + 'angularjs/angular.js',
+      basePath + 'bootstrap/'
+    ])
+    .pipe(uglify())
+    .pipe(gulp.dest(compiledPath));
 });
 
 gulp.task('default', function() {
