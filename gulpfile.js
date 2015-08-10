85949dd24643bb01c89ba05948cb64c079499f00
zehui.lv <zehui.lv@dianrong.com>
2015-08-10 18:17:32 +0800

    integret jade and angular

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,15 @@ var less = require('gulp-less');
 var uglify = require('gulp-uglify');
 var path = require('path');
 var concat = require('gulp-concat');
-
+var notify = require('gulp-notify');
+var ngAnnotate = require('gulp-ng-annotate');
+var liveReload = require('gulp-livereload');
 var compiledPath = './public/compiled';
+var watch = require('gulp-watch');
+var moment = require('moment');
 
 gulp.task('less', function() {
-  return gulp.src('./public/stylesheets/less/**/*.less')
+  return gulp.src(['./public/stylesheets/less/**/*.less'])
     .pipe(less({
       paths: [path.join(__dirname, 'less', 'includes')]
     }))
@@ -16,14 +20,50 @@ gulp.task('less', function() {
 
 gulp.task('thirdparty', function() {
   var basePath = './public/components/';
-  return gulp.src([basePath + 'jquery/jquery.js',
+  return gulp.src([
       basePath + 'angularjs/angular.js',
-      basePath + 'bootstrap/'
+      basePath + 'jquery/dist/jquery.js',
+      basePath + 'bootstrap/dist/js/bootstrap.js',
+      basePath + 'angularjs-route/angular-route.js'
     ])
-    .pipe(uglify())
+    .pipe(concat('thirdparty.js'))
     .pipe(gulp.dest(compiledPath));
 });
 
-gulp.task('default', function() {
-
+gulp.task('uglify-js', function() {
+  gulp.src(['./public/javascripts/**/*.js', './views/**/*.js'])
+    .pipe(concat('app.js'))
+    .pipe(ngAnnotate())
+    .on('error', notify.onError("Error: <%= error.message %>"))
+    //    .pipe(uglify())
+    .on('error', notify.onError("Error: <%= error.message %>"))
+    .pipe(gulp.dest(compiledPath))
+    .pipe(notify('Uglified JavaScript (' + moment().format('MMM Do h:mm:ss A') + ')'))
+    .pipe(liveReload({
+      auto: false
+    }));
 });
+
+gulp.task('watch', function() {
+  liveReload.listen();
+  watch({
+    glob: ['./views/**/*.js', './public/javascripts/**/*.js']
+  }, function() {
+    gulp.start('uglify-js');
+  });
+
+  watch({
+    glob: './public/stylesheets/**./*.less'
+  }, function() {
+    gulp.start('less');
+  });
+
+  watch({
+    glob: './views/**/*.html'
+  }).pipe(liveReload({
+    auto: false
+  }));
+});
+
+
+gulp.task('default', ['watch']);
---
+++ views/index.js
@@ -0,0 +1,16 @@
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function($scope) {
+  $scope.phones = [{
+    'name': 'Nexus',
+    'snippet': 'fast just. ...'
+  }, {
+    'name': 'Motorola',
+    'snippet': 'next gen...'
+  }, {
+    'name': 'motorola xoom',
+    'snippet': 'next tablet....'
+  }];
+
+  $scope.name = "World";
+});
