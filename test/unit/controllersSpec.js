d9bbffec97923b10c306b8b9e15c77c8c1fcf65c
zehui.lv <zehui.lv@dianrong.com>
2015-08-12 14:19:36 +0800

    step-5

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,16 +1,32 @@
-describe('PhoneListCtrl', function() {
-  beforeEach(module('phonecatApp'));
-  var scope, ctrl;
-  beforeEach(inject(function($controller) {
-    scope = {};
-    ctrl = $controller('PhoneListCtrl', {
-      $scope: scope
+describe('PhoneCat controllers', function() {
+  describe('PhoneListCtrl', function() {
+    beforeEach(module('phonecatApp'));
+    var scope, ctrl;
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('data/phones/phones.json').respond([{
+        name: 'Nexus S'
+      }, {
+        name: 'Motorola DROID'
+      }]);
+      scope = $rootScope.$new();
+
+      ctrl = $controller('PhoneListCtrl', {
+        $scope: scope
+      });
+    }));
+    it('should create "phones" model with 2 phones fetched from xhr', function() {
+      expect(scope.phones).toBeUndefined();
+      $httpBackend.flush();
+
+      expect(scope.phones).toEqual([{
+        name: 'Nexus S'
+      }, {
+        name: 'Motorola DROID'
+      }]);
     });
-  }));
-  it('should create "phones" model with 3 phones', inject(function($controller) {
-    expect(scope.phones.length).toBe(3);
-  }));
-  it('should set the default value of orderProp model', function() {
-    expect(scope.orderProp).toBe('age');
+    it('should set the default value of orderProp model', function() {
+      expect(scope.orderProp).toBe('age');
+    });
   });
 });
--- views/index.js
+++ views/index.js
@@ -1,17 +1,12 @@
 var phonecatApp = angular.module('phonecatApp', []);
-
-phonecatApp.controller('PhoneListCtrl', function($scope) {
-  $scope.phones = [{
-    'name': 'Nexus',
-    'snippet': 'fast just. ...'
-  }, {
-    'name': 'Motorola',
-    'snippet': 'next gen...'
-  }, {
-    'name': 'motorola xoom',
-    'snippet': 'next tablet....'
-  }];
-
-  $scope.name = "World";
+//Why the $scope and $http are needed in the array?
+//Because when I minify the js code for controllers,
+//all of functioin arguments would be minified as well,
+//and the dependency injector would not be able to
+//identify services correctly.
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('data/phones/phones.json').success(function(data) {
+    $scope.phones = data.splice(0, 5);
+  });
   $scope.orderProp = "age";
-});
+}]);
